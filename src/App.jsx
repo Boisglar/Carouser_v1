0c2e7ed04ab00c1e813528cdc122f24665d2a191
boisglar <dbaisangur95@gmail.com>
2022-12-03 18:33:43 +0300

    первая версия слайдера

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Carousel } from "./carousel/Carousel";
+import "./App.css"
+
+
+function App() {
+
+  return (
+    
+      <Carousel>
+        <div className="item item-1">item 1</div>
+        <div className="item item-2">item 2</div>
+        <div className="item item-3"> item 3</div>
+      </Carousel>
+
+  )
+}
+  
+
+export default App;
---
+++ src/carousel/Carousel.jsx
@@ -0,0 +1,59 @@
+import React, { Children, cloneElement, useEffect, useState } from 'react'
+import{ FaChevronLeft, FaChevronRight} from "react-icons/fa"
+
+import "./Carousel.css"
+
+const PAGE_WIDTH = 450
+
+export const Carousel = ({ children })  => {
+    const[ pages, setPages] = useState([])
+    const [offset, setOffSet] = useState(0)
+
+    const handeleLefArrowClick = () => {
+
+        setOffSet((currentOffSet)=> {
+            const newOffset = currentOffSet + PAGE_WIDTH
+            return Math.min(newOffset, 0)
+        })
+    };
+     
+    const handeleRightArrowClick = () => {
+
+        setOffSet((currentOffSet) => {
+            const newOffset = currentOffSet - PAGE_WIDTH
+            return Math.max(newOffset, -900 )
+         }) 
+    }
+
+
+    useEffect(()=>{
+        setPages(
+            Children.map(children, (child)=>{
+                return cloneElement(child, {
+                    style: {
+                        height:"100%",
+                        minWidth:`${PAGE_WIDTH}px`,
+                        maxWidth:`${PAGE_WIDTH}px`,
+                    },
+                })
+            })
+        )
+    }, [])
+
+
+
+
+  return (
+    <div className='main-container'>
+        <FaChevronLeft className='arrow' onClick={handeleLefArrowClick}/>
+        <div className='window'>
+            <div className='all-pages-container'
+            style={{
+                 transform: `translateX(${offset}px)`, 
+            }}
+            >{pages}</div>
+        </div>
+        <FaChevronRight className='arrow' onClick={handeleRightArrowClick} />
+    </div>
+  )
+}
